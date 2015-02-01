11f952c0a1ae85eed059aaf541e9dbfdc6b947ea
Po-Ying Chen <poying.me@gmail.com>
2015-02-01 23:08:12 +0800

    stream

--- examples/stream.js
+++ examples/stream.js
@@ -5,7 +5,7 @@ var zoom = require('../lib');
 zoom.connection('192.83.186.170:210/INNOPAC')
   .set('preferredRecordSyntax', 'usmarc')
   .query('prefix', '@attr 1=4 台灣')
-  .createReadStream()
+  .createReadStream({ start: 1, limit: 100 })
   .on('data', function (record) {
-    console.log(record.json);
+    console.log(record.json.leader);
   });
